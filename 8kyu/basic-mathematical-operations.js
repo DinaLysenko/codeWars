cf09795fd7a9a18a2df36aafdcffc141c01818a3
DinaLysenko <dinafate@rambler.ru>
2023-07-28 01:21:50 +0500

    basic-mathematical-operations2.js

--- 8kyu/basic-mathematical-operations.js
+++ 8kyu/basic-mathematical-operations.js
@@ -25,3 +25,8 @@ function basicOp(operation, value1, value2) {
         return value1 / value2
     }
 }
+
+console.log('basicOp: ', basicOp('+', 4, 7))
+console.log('basicOp: ', basicOp('-', 15, 18))
+console.log('basicOp: ', basicOp('*', 5, 5))
+console.log('basicOp: ', basicOp('/', 49, 7))
