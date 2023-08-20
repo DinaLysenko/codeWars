42c4e7ae38abad03387f7c326276156c3a0cde06
DinaLysenko <dinafate@rambler.ru>
2023-08-20 23:39:39 +0500

    bin to decimal

---
+++ 8kyu/bin-to-decimal.js
@@ -0,0 +1,7 @@
+// ❓DESCRIPTION:
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+// ✅SOLUTION:
+function binToDec(bin) {
+    let decimal = parseInt(bin, 2);
+    return decimal;
+}
