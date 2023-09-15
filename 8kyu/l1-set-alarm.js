9a44e5e3a284790d5dc1b3d3fe90c43ca11d09dd
DinaLysenko <dinafate@rambler.ru>
2023-09-15 22:28:03 +0500

    l1 set alarm

---
+++ 8kyu/l1-set-alarm.js
@@ -0,0 +1,17 @@
+// ❓DESCRIPTION:
+// Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+// employed | vacation
+// true     | true     => false
+// true     | false    => true
+// false    | true     => false
+// false    | false    => false
+
+// ✅SOLUTION:
+function setAlarm(employed, vacation) {
+    return !(employed === vacation || !employed);
+}
+
+console.log('setAlarm: ', setAlarm(setAlarm(true, true)))
+console.log('setAlarm: ', setAlarm(setAlarm(false,true)))
+console.log('setAlarm: ', setAlarm(setAlarm(true, false)))
